4af5dfeb50a805bf0d71dc598182afc8b3fc54f0
RP <x.ronny.08@gmail.com>
2020-08-14 15:19:47 -0400

    3 comimt

--- test_4/src/test_4/RMain.java
+++ test_4/src/test_4/RMain.java
@@ -5,7 +5,8 @@ public class RMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-			System.out.println("Hola github");
+			System.out.println("Hola github!!!!");
+			System.out.println("Hola github!!!!");
 	}
 
 }
